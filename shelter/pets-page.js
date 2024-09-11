6725e9633c82af9f3d8b3a31bdda35b59f5f5e59
ksenofonus <danilova.ka88@yandex.ru>
2024-09-11 09:32:56 +0400

    fix: fix our pets page js addresses

--- shelter/pets-page.js
+++ shelter/pets-page.js
@@ -1,3 +1,3 @@
-import '/assets/js/burger.js';
-import '/assets/js/pagination.js';
-import '/assets/js/popap.js';
+import './assets/js/burger.js';
+import './assets/js/pagination.js';
+import './assets/js/popap.js';
