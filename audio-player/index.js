2318285feab5d43e023d7705600cbac8cecd8af7
ksenofonus <danilova.ka88@yandex.ru>
2024-09-24 15:15:17 +0400

    feat: add image add function

--- audio-player/index.js
+++ audio-player/index.js
@@ -1,33 +1,56 @@
 let isPlay = false;
 let playNum = 0;
-let playList = ['./assets/audio/Cheek_to_Cheek.mp3', './assets/audio/Go_Down_Moses.mp3', './assets/audio/What_A_Wonderful_World.mp3','./assets/audio/beyonce (1).mp3', './assets/audio/dontstartnow.mp3'];
-let coverList = ['./assets/images/cheek-to-cheek.png', './assets/images/go_down.png', './assets/images/wonderfull_world.png', './assets/images/lemonade.png', './assets/images/dontstartnow.png'];
-let singerList = ['Louis Armstrong', 'Louis Armstrong', 'Louis Armstrong', 'Beyonce', 'Dua Lipa'];
-let titleList =['Cheek to Cheek', 'Go Down Moses', 'A Wonderful World', 'Don\'t hurt yourself', 'Don\'t stop now'];
+let playList = [
+  "./assets/audio/Cheek_to_Cheek.mp3",
+  "./assets/audio/Go_Down_Moses.mp3",
+  "./assets/audio/What_A_Wonderful_World.mp3",
+  "./assets/audio/beyonce (1).mp3",
+  "./assets/audio/dontstartnow.mp3",
+];
+let coverList = [
+  "./assets/images/cheek-to-cheek.png",
+  "./assets/images/go_down.png",
+  "./assets/images/wonderfull_world.png",
+  "./assets/images/lemonade.png",
+  "./assets/images/dontstartnow.png",
+];
+let singerList = [
+  "Louis Armstrong",
+  "Louis Armstrong",
+  "Louis Armstrong",
+  "Beyonce",
+  "Dua Lipa",
+];
+let titleList = [
+  "Cheek to Cheek",
+  "Go Down Moses",
+  "A Wonderful World",
+  "Don't hurt yourself",
+  "Don't stop now",
+];
 
-const audio = document.querySelector('audio');
-const playButton = document.querySelector('.play-button');
-const prevButton = document.querySelector('.prev');
-const nextButton = document.querySelector('.next');
-const background = document.querySelector('.background');
-const cover = document.querySelector('.audio__cover img');
-const singer = document.querySelector('.singer');
-const song = document.querySelector('.song');
-const songLength = document.querySelector('.audio-length');
-const progressBar = document.querySelector('.progressbar');
-const currentTime = document.querySelector('.curent');
-const timeline = document.querySelector('.timeline');
-
+const audio = document.querySelector("audio");
+const playButton = document.querySelector(".play-button");
+const prevButton = document.querySelector(".prev");
+const nextButton = document.querySelector(".next");
+const background = document.querySelector(".background");
+const cover = document.querySelector(".audio__cover img");
+const singer = document.querySelector(".singer");
+const song = document.querySelector(".song");
+const songLength = document.querySelector(".audio-length");
+const progressBar = document.querySelector(".progressbar");
+const currentTime = document.querySelector(".curent");
+const timeline = document.querySelector(".timeline");
 
 //play
-playButton.addEventListener('click', function playPause() {
+playButton.addEventListener("click", function playPause() {
   if (audio.paused) {
     // audio.currentTime = 0;
     audio.play();
-    playButton.classList.add('pause');
+    playButton.classList.add("pause");
   } else {
     audio.pause();
-    playButton.classList.remove('pause');
+    playButton.classList.remove("pause");
   }
   isPlay = !isPlay;
 });
@@ -35,82 +58,88 @@ playButton.addEventListener('click', function playPause() {
 
 //prev
 function changeContent() {
-  audio.setAttribute('src', playList[playNum]);
-  background.setAttribute('src', coverList[playNum]);
-  cover.setAttribute('src', coverList[playNum]);
+  audio.setAttribute("src", playList[playNum]);
+  background.setAttribute("src", coverList[playNum]);
+  cover.setAttribute("src", coverList[playNum]);
   singer.textContent = singerList[playNum];
   song.textContent = titleList[playNum];
 }
-  function prev() {
-    playNum = playNum - 1;
-    if (playNum < 0) {
-      playNum = playList.length - 1; //
-    } else if (playNum > (playList.length - 1)) {
-      playNum = 0;
-    }
-    changeContent();
+function prev() {
+  playNum = playNum - 1;
+  if (playNum < 0) {
+    playNum = playList.length - 1; //
+  } else if (playNum > playList.length - 1) {
+    playNum = 0;
+  }
+  changeContent();
 }
-prevButton.addEventListener('click', () => {
-    prev();
-    audio.play();
-    playButton.classList.add('pause');
-  });
+prevButton.addEventListener("click", () => {
+  prev();
+  audio.play();
+  playButton.classList.add("pause");
+});
 
 //next
 function next() {
   playNum = playNum + 1;
   if (playNum < 0) {
     playNum = playList.length - 1;
-  } else if (playNum > (playList.length - 1)) {
+  } else if (playNum > playList.length - 1) {
     playNum = 0;
   }
   changeContent();
 }
-nextButton.addEventListener('click', () => {
-    next();
-    audio.play();
-    playButton.classList.add('pause');
+nextButton.addEventListener("click", () => {
+  next();
+  audio.play();
+  playButton.classList.add("pause");
 });
 
 //ended track
 
-audio.addEventListener ('ended', () => {
+audio.addEventListener("ended", () => {
   next();
-  audio.play()});
+  audio.play();
+});
 
 //timecode
 function formatSecondsToTime(sec) {
-  let minutes = Math.floor(sec/60);
-  let seconds = Math.floor(sec - (minutes * 60));
+  let minutes = Math.floor(sec / 60);
+  let seconds = Math.floor(sec - minutes * 60);
 
-  if (minutes < 10){
+  if (minutes < 10) {
     minutes = "0" + minutes;
   }
-  if (seconds < 10){
-    seconds  = "0" + seconds;
+  if (seconds < 10) {
+    seconds = "0" + seconds;
   }
 
-  return minutes + ':' + seconds;
+  return minutes + ":" + seconds;
 }
-audio.addEventListener("loadeddata", () => {
+audio.addEventListener(
+  "loadeddata",
+  () => {
     songLength.textContent = formatSecondsToTime(audio.duration);
   },
-  false
+  false,
 );
 setInterval(() => {
-  progressBar.style.left = (audio.currentTime / audio.duration * 100 + "%");
+  progressBar.style.left = (audio.currentTime / audio.duration) * 100 + "%";
   currentTime.textContent = formatSecondsToTime(audio.currentTime);
 }, 1000);
 
 //timeline change
-timeline.addEventListener("click", (e) => {
-  const timelineWidth = window.getComputedStyle(timeline).width;
-  const timeChange = e.offsetX / parseInt(timelineWidth) * audio.duration;
-  audio.currentTime = timeChange;
-}, false);
+timeline.addEventListener(
+  "click",
+  (e) => {
+    const timelineWidth = window.getComputedStyle(timeline).width;
+    const timeChange = (e.offsetX / parseInt(timelineWidth)) * audio.duration;
+    audio.currentTime = timeChange;
+  },
+  false,
+);
 
-
-progressBar.onmousedown = function(e) {
+progressBar.onmousedown = function (e) {
   e.preventDefault();
   let shiftX = e.clientX - progressBar.getBoundingClientRect().left;
   function onMouseMove(e) {
@@ -123,17 +152,19 @@ progressBar.onmousedown = function(e) {
       newLeft = rightEdge;
     }
 
-    progressBar.style.left = newLeft + 'px';
+    progressBar.style.left = newLeft + "px";
   }
   function onMouseUp() {
-    document.removeEventListener('mouseup', onMouseUp);
-    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener("mouseup", onMouseUp);
+    document.removeEventListener("mousemove", onMouseMove);
   }
-  document.addEventListener('mousemove', onMouseMove);
-  document.addEventListener('mouseup', onMouseUp);
+  document.addEventListener("mousemove", onMouseMove);
+  document.addEventListener("mouseup", onMouseUp);
 };
-progressBar.ondragstart = function() {
+progressBar.ondragstart = function () {
   return false;
 };
 //self-test
-console.log('Вёрстка:\nвёрстка аудиоплеера: есть кнопка Play/Pause, кнопки "Вперёд" и "Назад" для пролистывания аудиотреков, прогресс-бар, отображается название и автор трека +5\nв футере приложения есть ссылка на гитхаб автора приложения, год создания приложения, логотип курса со ссылкой на курс +5\nКнопка Play/Pause\nесть кнопка Play/Pause, при клике по которой можно запустить или остановить проигрывание аудиотрека +5\nвнешний вид и функционал кнопки Play/Pause изменяется в зависимости от того, проигрывается ли в данный момент аудиотрек +5\nПри кликах по кнопкам "Вперёд" и "Назад" переключается проигрываемый аудиотрек. Аудиотреки пролистываются по кругу - после последнего идёт первый +10\nПри смене аудиотрека меняется изображение - обложка аудиотрека +10\nПрогресс-бар отображает прогресс проигрывания текущего аудиотрека. При перемещении ползунка вручную меняется текущее время проигрывания аудиотрека +10\nОтображается продолжительность аудиотрека и его текущее время проигрывания +10\nРезультат 60');
+console.log(
+  'Вёрстка:\nвёрстка аудиоплеера: есть кнопка Play/Pause, кнопки "Вперёд" и "Назад" для пролистывания аудиотреков, прогресс-бар, отображается название и автор трека +5\nв футере приложения есть ссылка на гитхаб автора приложения, год создания приложения, логотип курса со ссылкой на курс +5\nКнопка Play/Pause\nесть кнопка Play/Pause, при клике по которой можно запустить или остановить проигрывание аудиотрека +5\nвнешний вид и функционал кнопки Play/Pause изменяется в зависимости от того, проигрывается ли в данный момент аудиотрек +5\nПри кликах по кнопкам "Вперёд" и "Назад" переключается проигрываемый аудиотрек. Аудиотреки пролистываются по кругу - после последнего идёт первый +10\nПри смене аудиотрека меняется изображение - обложка аудиотрека +10\nПрогресс-бар отображает прогресс проигрывания текущего аудиотрека. При перемещении ползунка вручную меняется текущее время проигрывания аудиотрека +10\nОтображается продолжительность аудиотрека и его текущее время проигрывания +10\nРезультат 60',
+);
--- image-gallery/index.js
+++ image-gallery/index.js
@@ -1,8 +1,20 @@
-const url = 'https://api.unsplash.com/photos/?client_id=XcmGjG_wr1Hwy5aJ8E5mpCE3Omatp8NzKQJAwOlftZM';
+const url =
+  "https://api.unsplash.com/photos/?&per_page=20&orientation=landscape&client_id=XcmGjG_wr1Hwy5aJ8E5mpCE3Omatp8NzKQJAwOlftZM";
+const wrapper = document.querySelector(".image-wrapper");
 const getData = async () => {
   const result = await fetch(url);
   const data = await result.json();
-  return data;
-}
-const images = getData();
-console.log(images);
+  showcards(data);
+};
+getData();
+
+const showcards = (data) => {
+  for (let i = 0; i < data.length; i++) {
+		wrapper.insertAdjacentHTML(
+			"afterbegin",
+			`<div class="card"><img src="${data[i].urls.regular}" alt="${data[i].alt_description}"/></div>`,
+		);
+	}
+};
+
+
