db3e61136f7aac610cedf7f84af49c47946981b9
ksenofonus <danilova.ka88@yandex.ru>
2024-09-25 13:50:49 +0400

    feat: add clear button on search field, implement clear function

--- image-gallery/index.js
+++ image-gallery/index.js
@@ -1,6 +1,11 @@
 let url =
 	'https://api.unsplash.com/photos/?&per_page=21&orientation=landscape&client_id=XcmGjG_wr1Hwy5aJ8E5mpCE3Omatp8NzKQJAwOlftZM';
 const wrapper = document.querySelector('.image-wrapper');
+const field = document.getElementById('search_field');
+const searchBlock = document.querySelector('.search');
+const searchIcon = document.querySelector('.search_icon');
+const searchClear = document.querySelector('.search_clear');
+
 const getData = async () => {
 	try {
 		const result = await fetch(url);
@@ -9,10 +14,14 @@ const getData = async () => {
 
 		return data;
 	} catch {
-		alert('API cannot provide such data/ Please, try again')
+		alert('API cannot provide such data/ Please, try again');
 	}
 };
-document.addEventListener('DOMContentLoaded', getData);
+
+document.addEventListener('DOMContentLoaded', () => {
+	getData();
+	field.focus();
+});
 
 const showcards = (data) => {
 	for (let i = 0; i < data.length; i++) {
@@ -34,7 +43,6 @@ const showcards = (data) => {
 };
 
 const search = () => {
-	const field = document.getElementById('search_field');
 	const queryValue = field.value.toLowerCase();
 	url = `https://api.unsplash.com/search/photos?query=${queryValue}&per_page=21&orientation=landscape&client_id=XcmGjG_wr1Hwy5aJ8E5mpCE3Omatp8NzKQJAwOlftZM`;
 	getData();
@@ -43,5 +51,19 @@ const search = () => {
 document.addEventListener('keyup', (event) => {
 	if (event.code === 'Enter') {
 		search();
+	} else {
+		if (!searchClear.classList.contains('search_clear__active')) {
+			searchClear.classList.add('search_clear__active');
+		}
 	}
+});
+
+searchIcon.addEventListener('click', () => {
+	search();
+});
+
+searchClear.addEventListener('click', () => {
+	field.value = '';
+	searchClear.classList.remove('search_clear__active');
+	field.focus();
 });
