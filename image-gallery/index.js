4f3900f3d0a5f7ef0b3979a03874b1a8db1ad9fe
ksenofonus <danilova.ka88@yandex.ru>
2024-09-25 07:18:11 +0400

    feat: add search function without moving

--- image-gallery/index.js
+++ image-gallery/index.js
@@ -1,12 +1,16 @@
-const url =
+let url =
   "https://api.unsplash.com/photos/?&per_page=21&orientation=landscape&client_id=XcmGjG_wr1Hwy5aJ8E5mpCE3Omatp8NzKQJAwOlftZM";
 const wrapper = document.querySelector(".image-wrapper");
 const getData = async () => {
   const result = await fetch(url);
   const data = await result.json();
   showcards(data);
+  if (data.hasOwnProperty('results')) {
+    console.log(data.results)
+    }
+  return data;
 };
-getData();
+document.addEventListener('DOMContentLoaded', getData());
 
 const showcards = (data) => {
   for (let i = 0; i < data.length; i++) {
@@ -15,4 +19,21 @@ const showcards = (data) => {
       `<div class="card"><img src="${data[i].urls.regular}" alt="${data[i].alt_description}"/></div>`,
     );
   }
+
 };
+
+
+
+const search = () => {
+  const field = document.getElementById('search_field');
+  const queryValue = field.value.toLowerCase();
+  url = `https://api.unsplash.com/search/photos?query=${queryValue}&per_page=21&orientation=landscape&client_id=XcmGjG_wr1Hwy5aJ8E5mpCE3Omatp8NzKQJAwOlftZM`;
+  console.log(url);
+  getData();
+}
+
+document.addEventListener('keyup', (event) => {
+  if (event.code === 'Enter') {
+    search();
+  };
+})
