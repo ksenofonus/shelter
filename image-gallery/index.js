c44b43a4bb579fd58f96c37c2f10c6792c635865
ksenofonus <danilova.ka88@yandex.ru>
2024-09-25 10:24:24 +0400

    feat: add error catch

--- image-gallery/index.js
+++ image-gallery/index.js
@@ -1,43 +1,47 @@
 let url =
-	"https://api.unsplash.com/photos/?&per_page=21&orientation=landscape&client_id=XcmGjG_wr1Hwy5aJ8E5mpCE3Omatp8NzKQJAwOlftZM";
-const wrapper = document.querySelector(".image-wrapper");
+	'https://api.unsplash.com/photos/?&per_page=21&orientation=landscape&client_id=XcmGjG_wr1Hwy5aJ8E5mpCE3Omatp8NzKQJAwOlftZM';
+const wrapper = document.querySelector('.image-wrapper');
 const getData = async () => {
-	const result = await fetch(url);
-	const data = await result.json();
-	showcards(data);
+	try {
+		const result = await fetch(url);
+		const data = await result.json();
+		showcards(data);
 
-	return data;
+		return data;
+	} catch {
+		alert('API cannot provide such data/ Please, try again')
+	}
 };
-document.addEventListener("DOMContentLoaded", getData);
+document.addEventListener('DOMContentLoaded', getData);
 
 const showcards = (data) => {
 	for (let i = 0; i < data.length; i++) {
 		wrapper.insertAdjacentHTML(
-			"afterbegin",
+			'afterbegin',
 			`<div class="card"><img src="" alt=""/></div>`,
 		);
 	}
-	const cards = wrapper.querySelectorAll("img");
+	const cards = wrapper.querySelectorAll('img');
 	cards.forEach((card, index) => {
-		if (data.hasOwnProperty("results")) {
-			card.setAttribute("src", `${data.results[index].urls.regular}`);
-			card.setAttribute("alt", `${data.results[index].alt_description}`);
+		if (data.hasOwnProperty('results')) {
+			card.setAttribute('src', `${data.results[index].urls.regular}`);
+			card.setAttribute('alt', `${data.results[index].alt_description}`);
 		} else {
-			card.setAttribute("src", `${data[index].urls.regular}`);
-			card.setAttribute("alt", `${data[index].alt_description}`);
+			card.setAttribute('src', `${data[index].urls.regular}`);
+			card.setAttribute('alt', `${data[index].alt_description}`);
 		}
 	});
 };
 
 const search = () => {
-	const field = document.getElementById("search_field");
+	const field = document.getElementById('search_field');
 	const queryValue = field.value.toLowerCase();
 	url = `https://api.unsplash.com/search/photos?query=${queryValue}&per_page=21&orientation=landscape&client_id=XcmGjG_wr1Hwy5aJ8E5mpCE3Omatp8NzKQJAwOlftZM`;
 	getData();
 };
 
-document.addEventListener("keyup", (event) => {
-	if (event.code === "Enter") {
+document.addEventListener('keyup', (event) => {
+	if (event.code === 'Enter') {
 		search();
 	}
 });
