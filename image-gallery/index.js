7f31aed7ea1ed221239a4c8da60847d34ae241a9
ksenofonus <danilova.ka88@yandex.ru>
2024-09-25 09:14:26 +0400

    refactor: refactor show cards function

--- image-gallery/index.js
+++ image-gallery/index.js
@@ -5,7 +5,7 @@ const getData = async () => {
 	const result = await fetch(url);
 	const data = await result.json();
 	showcards(data);
-	
+
 	return data;
 };
 document.addEventListener("DOMContentLoaded", getData);
@@ -17,25 +17,22 @@ const showcards = (data) => {
 			`<div class="card"><img src="" alt=""/></div>`,
 		);
 	}
-	const cards = wrapper.querySelectorAll('img');
-	if (data.hasOwnProperty("results")) {
-		cards.forEach((card, index) => {
-			card.setAttribute('src', `${data.results[index].urls.regular}`);
-			card.setAttribute('alt', `${data.results[index].alt_description}`);
-		});
-	} else {
-    cards.forEach((card, index) => {
+	const cards = wrapper.querySelectorAll("img");
+	cards.forEach((card, index) => {
+		if (data.hasOwnProperty("results")) {
+			card.setAttribute("src", `${data.results[index].urls.regular}`);
+			card.setAttribute("alt", `${data.results[index].alt_description}`);
+		} else {
 			card.setAttribute("src", `${data[index].urls.regular}`);
 			card.setAttribute("alt", `${data[index].alt_description}`);
-		});
-  }
+		}
+	});
 };
 
 const search = () => {
 	const field = document.getElementById("search_field");
 	const queryValue = field.value.toLowerCase();
 	url = `https://api.unsplash.com/search/photos?query=${queryValue}&per_page=21&orientation=landscape&client_id=XcmGjG_wr1Hwy5aJ8E5mpCE3Omatp8NzKQJAwOlftZM`;
-	console.log(url);
 	getData();
 };
 
